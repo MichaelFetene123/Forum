88ac9504c7a2e81ba7520c90fc667428783a429b
MichaelFetene123 <michaelfetene0202@gmail.com>
2025-11-10 16:21:26 +0300

    header
    some header part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+import Header from './Components/Header';
 function App() {
   return (
     <>
-      <h1 className=" "> hello world</h1>
+   <Header />
     </>
   );
 }
---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,46 @@
+import React,{useState, useEffect} from 'react';
+import {Link, useNavigate } from "react-router-dom";
+
+const Header = ({logout=()=>{}}) => {
+
+    const [sticky, setSticky] = useState(false);
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token")
+
+
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            setSticky(window.scrollY > 1);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll)
+     }, []);
+  
+     const onClickChange = () =>{
+        if(token){
+            logout()
+            localStorage.removeItem("token")
+            navigate("/login")
+        }
+        if(!token){
+            navigate("/")
+        }
+     }
+
+     const logPage = () =>{
+        navigate('/login')
+     }
+  return (<>
+  <header className>
+<div>
+
+</div>
+  </header>
+  </>
+  )
+}
+
+export default Header
+
