c3c2ba9e363bea9e79303c64b2c29ea0ab9682fa
MichaelFetene123 <michaelfetene0202@gmail.com>
2025-11-19 19:30:11 +0300

    Add routing and registration form to frontend
    Introduced React Router for page navigation in App.jsx, added Home page, and implemented a registration form in Registor.jsx. Also added axios as a dependency for future API requests.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import About from './Components/About/about';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Registor from './pages/Registor/Registor';
 function App() {
   return (
-    <>
-   <Header />
-   <About />
-   <Footer />
-    </>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Registor' element={<Registor />} />
+        <Route path='/footer' element={<Footer />} />
+    </Routes>
   );
 }
 
--- frontend/src/pages/Registor/Registor.jsx
+++ frontend/src/pages/Registor/Registor.jsx
@@ -3,8 +3,36 @@ import styles from './Registor.module.css'
 
 const Registor = () => {
   return (
-    <div>Registor</div>
-  )
+    <section>
+      <form action="">
+        <div>
+          <span>username :-- </span>
+          <input type="text" placeholder="username" />
+        </div>
+        <br />
+        <div>
+          <span>First Name :-- </span>
+          <input type="text" placeholder="first name" />
+        </div>
+        <br />
+        <div>
+          <span>Last Name :-- </span>
+          <input type="text" placeholder="last name" />
+        </div>
+        <br />
+        <div>
+          <span>email :-- </span>
+          <input type="email" placeholder="email" />
+        </div>
+        <br />
+        <div>
+          <span>Password :-- </span>
+          <input type="password" placeholder="password" />
+        </div>
+        <button type="submit">Registor</button>
+      </form>
+    </section>
+  );
 }
 
 export default Registor
