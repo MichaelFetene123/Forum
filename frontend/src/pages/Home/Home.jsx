dd8928730c00de3e58c5b896902e279e33674c8a
MichaelFetene123 <michaelfetene0202@gmail.com>
2025-11-26 21:09:54 +0300

    Update Home page layout and add Ask Question link
    Refactored the Home page to use a flex container for improved layout and added a prominent 'Ask Question' button linking to the ask-question route. Also updated the welcome message styling and made minor adjustments to index.html body class.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,15 +2,22 @@ import React,{useContext} from 'react'
 import { AppState } from '../../App'
 import classes from './Home.module.css'
 import QuestionCard from './QuestionCard';
+import { Link } from 'react-router-dom';
 const Home = () => {
   const { user, setUser } = useContext(AppState);
   // console.log(user.username);
   return (
-    <div className={classes.home}>
-      {" "}
-      <h2>Home</h2>
-      <br />
-      <h2>Welcome, {user?.username}</h2>
+    <div className="container  ">
+      <div className="flex justify-between m-10 mt-0">
+        <Link
+          to={"/ask-question"}
+          style={{ backgroundColor: "#516cf0" }}
+          className=" border-none  hover:bg-orange-500"
+        >
+          Ask Question
+        </Link>
+        <div>Welcome: {user?.username}</div>
+      </div>
       <QuestionCard />
     </div>
   );
