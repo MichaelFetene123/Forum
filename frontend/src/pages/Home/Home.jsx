af18f0801ea757faeddd9f89bd41e07cf08d203e
MichaelFetene123 <michaelfetene0202@gmail.com>
2025-11-27 12:38:34 +0300

    home page managing

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,26 +1,52 @@
-import React,{useContext} from 'react'
-import { AppState } from '../../App'
-import classes from './Home.module.css'
-import QuestionCard from './QuestionCard';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppState } from "../../App";
+import classes from "./Home.module.css";
+import QuestionCard from "./QuestionCard";
+
 const Home = () => {
-  const { user, setUser } = useContext(AppState);
-  // console.log(user.username);
+  const { user } = useContext(AppState);
+  contt [qdata, setQdata] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+
+
+  
+  const filteredQuestions = useMemo(() => {
+    if(!searchQuery.trim()) return qdata;
+  })
   return (
-    <div className="container  ">
+    <div className="container">
       <div className="flex justify-between m-10 mt-0">
         <Link
-          to={"/ask-question"}
-          style={{ backgroundColor: "#516cf0" }}
-          className=" border-none  hover:bg-orange-500"
+          to="/ask-question"
+          
+          className="border-none bg-blue-500 hover:bg-orange-500 pt-2 px-4 rounded-xl text-white"
         >
           Ask Question
         </Link>
         <div>Welcome: {user?.username}</div>
+        <div className="w-full max-w-sm">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full rounded-lg border px-4 py-2"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
       </div>
+      {searchQuery && <h3>Search Results</h3>}
+      <hr />
+      {
+        searchQuery && (
+
+        )
+      }
+
       <QuestionCard />
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
